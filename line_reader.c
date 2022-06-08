58775b01821644de1bb43e8b3871c1cf98ccfa4e
ies <ies@ies-labor>
2022-06-08 23:14:46 +0200

    compile-script

--- line_reader.c
+++ line_reader.c
@@ -2,10 +2,10 @@
 #include "iesusart.h"
 
 enum bool_t { false, true }; 
-typedef bool_t bool;
+typedef enum bool_t bool;
 
 int main(void) {
-	
+
     DDRC &= ~((1<<DDC0) | (1<<DDC1) | (1<<DDC2));	// read from PC0-2
     DDRD |= (1 << DDD4);							// write to PD4
     DDRB |= (1 << DDB2);							// write to PB2
@@ -13,5 +13,5 @@ int main(void) {
 
 	while (1) {
 		bool clk = (DDRD & (1 << DDD4));
-	}		
+	}
 }
