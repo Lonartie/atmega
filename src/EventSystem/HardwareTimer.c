48c0c1c2aa66422dee6313e5c9eb67f4627d6d3d
Lonartie <gierschner.lg@gmail.com>
2022-06-10 23:52:23 +0200

    timer try #4

--- src/EventSystem/HardwareTimer.c
+++ src/EventSystem/HardwareTimer.c
@@ -1,16 +1,16 @@
 #include "HardwareTimer.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/atomic.h>
 
 static volatile uint64_t timerMillis;
 
 void initTimer()
 {
-  TCCR1B = (1 << WGM12);       // from the datasheet
-  OCR1A = F_CPU / 1024 / 1000; // number of ticks in a second
-  
-  TIMSK1 = (1 << OCIE1A);      // from the data sheet
-  TCCR1B |= (1 << CS12) | (1 << CS10);
+	TCCR0A |= (1<<WGM01); // set CTC mode
+	TCCR0B |= ((1<<CS01) | (1<<CS00)); //set counting clock to F_CPU / 8 -> 1MHZ
+	TIMSK0 |= (1<<OCIE0A);
+	OCR0A = 125; // set counter compare val,we count from 0-99 w 1MHZ CLK
 }
 
 ISR(TIMER1_COMPA_vect) 
@@ -20,10 +20,9 @@ ISR(TIMER1_COMPA_vect)
 
 uint32_t millis()
 {
-	uint32_t m;
-	uint8_t oldSREG = SREG;
-	cli();
-	m = timerMillis;
-	SREG = oldSREG;
-	return m;
+  unsigned long  ms;	
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+		ms = timerMillis;
+	}	
+	return ms;
 }
