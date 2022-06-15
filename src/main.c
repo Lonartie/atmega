663b568b6c6e1be522bfed60a54e1a2de4f6c2a9
Lonartie <gierschner.lg@gmail.com>
2022-06-15 19:16:04 +0200

    drive when middle sensor

--- src/main.c
+++ src/main.c
@@ -1,23 +1,48 @@
 #include "Misc/utils.h"
+#include "EventSystem/EventSystem.h"
+#include "EventSystem/SensorWatcher.h"
 #include "EventSystem/HardwareTimer.h"
 #include "Models/PWM.h"
 #include "System.h"
 #include <stdio.h>
 #include <util/delay.h>
 
+void middle(void* t);
+
 int main()
 {
 	timer_init();
 	PWM_init();
 
+	EventSystem* system = EventSystem_instance();
 	System atmega = System_create();
-	ACTOR(atmega.mt_left).set_speed(155);
-	ACTOR(atmega.mt_right).set_speed(155);
-	ACTOR(atmega.mt_left).drive_forward();
-	ACTOR(atmega.mt_right).drive_backward();
 
-	_delay_ms(5000);
+	SensorWatcher watcher = SensorWatcher_create(atmega.lf_middle, "middle");
 
-	ACTOR(atmega.mt_left).stop();
-	ACTOR(atmega.mt_right).stop();
+	ACTOR_SCOPE(*system)
+	{
+		system->reg_listener(Listener_create_r(&atmega, middle, watcher.event));
+		ACTOR(watcher).start();
+		system->run();
+	}
+}
+
+void middle(void* t)
+{
+	static bool drive = false;
+
+	drive = !drive;
+	System* atmega = (System*) t;
+	if (drive)
+	{
+		ACTOR(atmega->mt_left).set_speed(155);
+		ACTOR(atmega->mt_right).set_speed(155);
+		ACTOR(atmega->mt_left).drive_forward();
+		ACTOR(atmega->mt_right).drive_forward();
+	}
+	else
+	{
+		ACTOR(atmega->mt_left).stop();
+		ACTOR(atmega->mt_right).stop();
+	}
 }
