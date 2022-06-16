c5bb702d3177576f9dbe4da460b52317ff59ee64
Lonartie <gierschner.lg@gmail.com>
2022-06-16 10:04:34 +0000

    test#13

--- src/Models/Motor.c
+++ src/Models/Motor.c
@@ -26,14 +26,18 @@ Motor Motor_create(uint8_t pwm_pin, Pin forward, Pin backward)
 
 void Motor_drive_forward(Motor* _this, uint8_t speed)
 {
-  Pin_write(&_this->backward, 0); _delay_ms(1);
+  Pin_write(&_this->forward, 0);
+  Pin_write(&_this->backward, 0); 
+  _delay_ms(1);
   Pin_write(&_this->forward, 1);
   PWM_set_duty_cycle(_this->pwm_pin, speed);
 }
 
 void Motor_drive_backward(Motor* _this, uint8_t speed)
 {
-  Pin_write(&_this->forward, 0); _delay_ms(1);
+  Pin_write(&_this->forward, 0);
+  Pin_write(&_this->backward, 0);
+   _delay_ms(1);
   Pin_write(&_this->backward, 1);
   PWM_set_duty_cycle(_this->pwm_pin, speed);
 }
