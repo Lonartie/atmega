8518cb7ec7ab09afac4c75cb006f8633e63ad0f5
Lonartie <gierschner.lg@gmail.com>
2022-06-22 23:04:18 +0200

    usart event #3

--- src/EventSystem/USARTEvent.c
+++ src/EventSystem/USARTEvent.c
@@ -1,10 +1,31 @@
 #include "USARTEvent.h"
 #include "../Misc/Debug.h"
+#include "../Models/USART.h"
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h> 
 
+static USART usart;
+static bool initialized = false;
+
+USARTEvent USARTEvent_create()
+{
+  if (!initialized) {
+    usart = USART_create();
+    initialized = true;
+  }
+
+  USARTEvent event;
+  return event;
+}
+
 ISR(USART_RX_vect)
 {
-  debug("USART event? help!");
+  char data[64];
+  uint8_t i = 0;
+  do {
+    data[i] = USART_recv_byte(&usart);
+  } while(data[i++] != '\r');
+
+  debug(data);
 }
--- src/EventSystem/USARTEvent.h
+++ src/EventSystem/USARTEvent.h
@@ -2,7 +2,9 @@
 #define USART_EVENT_H
 
 typedef struct USARTEvent {
-  
+
 } USARTEvent;
+
+USARTEvent USARTEvent_create();
 
 #endif // USART_EVENT_H
