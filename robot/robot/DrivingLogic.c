02b02d8367da6b3044afd4483210abdecc8769b3
Lonartie <gierschner.lg@gmail.com>
2022-07-06 18:57:25 +0200

    drive around obstacle #12

--- robot/robot/DrivingLogic.c
+++ robot/robot/DrivingLogic.c
@@ -124,20 +124,20 @@ void drive_logic(System* atmega) {
     wall_phase = 1;
     return;
   } else if (wall_phase == 1) {
-    Menu_log(LOG_INFO, FMT("wall: %d", (int)wall_detected));
+    Menu_log(LOG_INFO, FMT("wall: %d\n", (int)wall_detected));
+    if (mid) {
+      turn_right(atmega, may_log);
+      wall_phase = 0;
+    }
+
     if (wall_detected) {
       Menu_log(LOG_INFO, "phase 1 fw\n");
       drive_forward(atmega, may_log);
       return;
     } else {
-      Menu_log(LOG_INFO, "phase 1 tr\n");
+      Menu_log(LOG_INFO, "phase 1 tl\n");
       turn_left(atmega, may_log);
       return;
-    }
-
-    if (mid) {
-      turn_right(atmega, may_log);
-      wall_phase = 0;
     }
   }
 
