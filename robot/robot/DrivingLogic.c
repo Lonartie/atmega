a7990b6bd0f65426817390b9ca1391c8dcdc4b8e
Lonartie <gierschner.lg@gmail.com>
2022-07-12 18:23:43 +0200

    wall logic again #52

--- robot/robot/DrivingLogic.c
+++ robot/robot/DrivingLogic.c
@@ -55,7 +55,7 @@ void detect_wall(void* system) {
     return;
   }
 
-  if (((uint32_t)micros() - last_t) <= 100000) {
+  if (((uint32_t)micros() - last_t) <= 10000) {
     wall_detected = true;
   } else {
     wall_detected = false;
