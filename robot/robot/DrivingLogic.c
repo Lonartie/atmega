92b226641ade72e8dd1d5b423ea0bfa63935ce8d
Lonartie <gierschner.lg@gmail.com>
2022-07-12 18:17:41 +0200

    wall logic again #46

--- robot/robot/DrivingLogic.c
+++ robot/robot/DrivingLogic.c
@@ -50,13 +50,12 @@ void detect_wall(void* system) {
   static uint16_t last_t = 0;
   System* atmega = (System*)system;
 
-  Menu_log(LOG_INFO, "d\n");
-
   if ((UltraSoundSensor_dist(&atmega->us) > US_SENSOR_DISTANCE)) {
     wall_detected = false;
     return;
   }
 
+  Menu_log(LOG_INFO, "d\n");
   if (micros() - last_t <= 1000000) {
     wall_detected = true;
   } else {
