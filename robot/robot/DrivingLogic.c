c6f2a2f6f463d9262f206d41ffe095f197c13fd1
Lonartie <gierschner.lg@gmail.com>
2022-07-13 17:10:45 +0200

    next day try #29

--- robot/robot/DrivingLogic.c
+++ robot/robot/DrivingLogic.c
@@ -11,9 +11,9 @@
 
 const int8_t SPEED_DRIVE_SLOW = 0;
 const int16_t SPEED_DRIVE = 170;
-const int16_t SPEED_TURN_A = 150;
-const int16_t SPEED_TURN_B = 150;
-const int16_t SPEED_TURN_SLOW_A = 190;
+const int16_t SPEED_TURN_A = 190;
+const int16_t SPEED_TURN_B = 190;
+const int16_t SPEED_TURN_SLOW_A = 200;
 const int16_t SPEED_TURN_SLOW_B = 0;
 
 static uint8_t US_SENSOR_DISTANCE = 13;
