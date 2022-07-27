37dc9b59bb3f1dc8c12890471cbb0530356e934d
Lonartie <gierschner.lg@gmail.com>
2022-07-27 18:29:53 +0200

    program flow #18

--- robot/core/EventSystem/HardwareTimer.c
+++ robot/core/EventSystem/HardwareTimer.c
@@ -4,6 +4,6 @@
 
 void timer_init() { hw_micros_timer_init(); }
 
-uint64_t millis() { return hw_micros() / 1000; }
+uint64_t millis() { return (micros() / 1000); }
 
 uint64_t micros() { return hw_micros(); }
