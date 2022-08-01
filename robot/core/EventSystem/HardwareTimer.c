489456e8c939b557fb8d4a73052d85df0fbc9715
Lonartie <gierschner.lg@gmail.com>
2022-08-01 10:15:03 +0200

    reset test

--- robot/core/EventSystem/HardwareTimer.c
+++ robot/core/EventSystem/HardwareTimer.c
@@ -1,30 +1,9 @@
 #include "HardwareTimer.h"
 
-#include <avr/interrupt.h>
-#include <avr/io.h>
-#include <avr/power.h>
-#include <util/atomic.h>
+#include "wraps/HWMicroTimer.h"
 
-#define MICROS_RESOLUTION 100
-#define PRESCALER 8
-
-static volatile uint64_t microseconds;
-
-void timer_init() {
-  // initialize timer 1 for microseconds counting
-  TCCR1A = 0;
-  TCCR1B = _BV(WGM12) | _BV(CS11);
-  TIMSK1 = _BV(OCIE1A);
-  OCR1A = ((F_CPU / PRESCALER) / 10000) - 1;
-  sei();
-}
+void timer_init() { hw_micros_timer_init(); }
 
 uint64_t millis() { return (micros() / 1000); }
 
-uint64_t micros() {
-  uint64_t us;
-
-  ATOMIC_BLOCK(ATOMIC_RESTORESTATE) { us = microseconds; }
-
-  return us;
-}
+uint64_t micros() { return hw_micros(); }
