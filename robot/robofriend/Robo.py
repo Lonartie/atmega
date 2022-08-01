bdc41714359c7e0e6405c8570ed63378467c18ad
Lonartie <gierschner.lg@gmail.com>
2022-08-01 18:19:04 +0200

    py gui #21

--- robot/robofriend/Robo.py
+++ robot/robofriend/Robo.py
@@ -6,8 +6,19 @@ class Robo:
 
     def __init__(self):
         self.con = serial.Serial(
-            port='/dev/rfcomm0', baudrate=9600, timeout=0.25)
+            port='/dev/rfcomm0', baudrate=9600, timeout=0.25,
+            parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, )
         self.con.write(('HelloWorld\r').encode('ascii', 'replace'))
+        self.con.flushOutput()
+
+        buffer = ""
+        while True:
+            character = self.con.read(1)
+            if (character != '\r' and character != '\n'):
+                buffer += character
+            else:
+                break
+        self.answer = buffer
 
     def close(self):
         self.con.close()
