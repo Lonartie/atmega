e2d18db16b2a3fcda7347604a3a5aca27340d39b
Lonartie <gierschner.lg@gmail.com>
2022-08-01 10:27:21 +0200

    ref #5 final #1

--- robot/core/EventSystem/HardwareTimer.c
+++ robot/core/EventSystem/HardwareTimer.c
@@ -1,23 +1,29 @@
 #include "HardwareTimer.h"
 
-#include "libs/micros.h"
+#include <avr/interrupt.h>
+#include <avr/io.h>
+#include <util/atomic.h>
+
+#define MICROS_RESOLUTION 100
+#define PRESCALER 8
+static volatile uint64_t microseconds = 0;
 
 void timer_init() {
   TCCR1A = 0;
   TCCR1B = _BV(WGM12) | (_BV(CS11));
   TIMSK1 = (1 << (1));
-  OCR1A = ((F_CPU / 8) / 10000) - 1;
+  OCR1A = ((F_CPU / PRESCALER) / 10000) - 1;
   sei();
 }
 
 uint64_t millis() { return (micros() / 1000); }
 
 uint64_t micros() {
-  micros_t us;
+  uint64_t us;
 
   ATOMIC_BLOCK(ATOMIC_RESTORESTATE) { us = microseconds; }
 
   return us;
 }
 
-ISR(ISR_VECT) { microseconds += MICROS_RESOLUTION; }
+ISR(TIMER1_COMPA_vect) { microseconds += MICROS_RESOLUTION; }
