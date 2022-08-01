365d6bf7ad0a04e907d1c48b0e95d0c02daf754f
Lonartie <gierschner.lg@gmail.com>
2022-08-01 14:55:28 +0200

    fix#2

--- robot/robot/Driving.c
+++ robot/robot/Driving.c
@@ -3,7 +3,7 @@
 #include "Constants.h"
 #include "EventSystem/HardwareTimer.h"
 #include "ObstacleAvoidance.h"
-#include "REset.h"
+#include "Reset.h"
 #include "States.h"
 
 void turn_left(System* atmega) {
