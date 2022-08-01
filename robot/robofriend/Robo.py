aeda196b64f4837300dedc22d7cfa84ef74f07c7
Lonartie <gierschner.lg@gmail.com>
2022-08-01 17:35:54 +0200

    py gui #3

--- robot/robofriend/Robo.py
+++ robot/robofriend/Robo.py
@@ -6,17 +6,9 @@ class Robo:
     def __init__(self):
         self.con = serial.Serial(
             port='/dev/rfcomm0', baudrate=9600, timeout=0.25)
-        self.con.write(b'?\r')
+        self.con.write(b'PY\r')
         self.con.flush()
-        answer = ""
-        done = False
-        while not done:
-            ch = self.con.read(1)
-            if (ch == None):
-                done = True
-            else:
-                answer += str(ch)
-        self.answer = answer
+        self.answer = str(self.con.readline())
 
     def close(self):
         self.con.close()
