a419eb213801ad2f849cffe8fbf5315d1156ce2f
Lonartie <gierschner.lg@gmail.com>
2022-08-01 18:10:40 +0200

    py gui #20

--- robot/robofriend/Robo.py
+++ robot/robofriend/Robo.py
@@ -7,8 +7,7 @@ class Robo:
     def __init__(self):
         self.con = serial.Serial(
             port='/dev/rfcomm0', baudrate=9600, timeout=0.25)
-        self.con.write(('PY\r').encode('ascii', 'replace'))
-        self.answer = self.con.readline().decode('ascii')
+        self.con.write(('HelloWorld\r').encode('ascii', 'replace'))
 
     def close(self):
         self.con.close()
